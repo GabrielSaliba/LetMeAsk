7598b4256dc05d7e8155085fbc16afc3430ea652
GabrielSaliba <gabriel.saliba.179@gmail.com>
2022-05-30 20:31:03 -0300

    ➕ - add types packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Home } from "./pages/Home";
+// import { Home } from "./pages/Home";
+import { Route } from 'react-router-dom'
 import { NewRoom } from "./pages/NewRoom";
 
 function App() {
